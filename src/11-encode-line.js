91801b8bda1c3653d72ac60417bc90697a90b4c6
Dzmitry92 <teramisu01@gmail.com>
2021-04-08 23:58:07 +0300

    feat: add solution for encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  if (str.length > 0) {
+    const arr = [];
+    let quantity = 0;
+    let char = str[0];
+    str.split('').forEach((elem) => {
+      if (elem === char) {
+        quantity += 1;
+      } else {
+        arr.push(`${quantity}${char}`);
+        quantity = 1;
+        char = elem;
+      }
+    });
+    arr.push(`${quantity}${char}`);
+    result = arr.map((elem) => elem.replace('1', '')).join('');
+  }
+  return result;
 }
 
 module.exports = encodeLine;
